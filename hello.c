78ee8561062819d590a94ffc9ad3129897315465
blackpearl <531834882@qq.com>
2014-06-21 11:41:31 +0800

    first commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: blackpearl
+	> Mail:531834882@qq.com 
+	> Created Time: 2014年06月21日 星期六 11时38分36秒
+ ************************************************************************/
+
+#include<stdio.h>
+void main()
+{
+	printf("xxxxxxxxxxxxxxxxxx,world!");
+	return 0;
+}
